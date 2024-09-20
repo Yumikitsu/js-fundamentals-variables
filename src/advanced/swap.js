72b414acf5e8096e1d72a5fde841dc439ff2542e
Joaquin Lindkvist <joaquinzache@gmail.com>
2024-09-20 09:23:21 +0200

    All extensions completed!

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -1,6 +1,10 @@
-const a = 8
-const b = 10
+let a = 8
+let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
+
+b = a + b // 8 + 10 = 18
+a = b - a // 18 - 8 = 10
+b = b - a // 18 - 10 = 8
 
 module.exports = { a, b }
